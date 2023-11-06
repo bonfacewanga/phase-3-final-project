6d01aaec71093770b56b29ac2753e91f31711e54
Bonface <bonniewanga34@gmail.com>
2023-11-06 08:40:21 +0300

    Setting up the algorithm.py file

--- project/algorithm.py
+++ project/algorithm.py
@@ -1,18 +1,18 @@
-# class Algorithms:
-#     @staticmethod
-#     def sample_algorithm(input_data):
-#         """
-#         This is a sample algorithm that doubles each element in the input list.
-#         Args:
-#             input_data (list): A list of input data for the algorithm.
+class Algorithms:
+    @staticmethod
+    def sample_algorithm(input_data):
+        """
+        This is a sample algorithm that doubles each element in the input list.
+        Args:
+            input_data (list): A list of input data for the algorithm.
         
-#         Returns:
-#             output_data: The result of the algorithm processing the input data.
-#         """
-#         output_data = []
+        Returns:
+            output_data: The result of the algorithm processing the input data.
+        """
+        output_data = []
 
-#         # Sample algorithm logic (doubling each element in the input list)
-#         for item in input_data:
-#             output_data.append(item * 2)
+        # Sample algorithm logic (doubling each element in the input list)
+        for item in input_data:
+            output_data.append(item * 2)
 
-#         return output_data
+        return output_data
